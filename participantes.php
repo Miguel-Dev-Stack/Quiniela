5cdaee3395e63ac070521298c44a00a9e2458de1
Miguel <mnavadiaz3@gmail.com>
2025-11-13 10:53:28 -0600

    Cambió de ubicacion local

---
+++ participantes.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiniela Liga MX | Participantes</title>
+    <link rel="preload" href="css/normalize.css" as="style">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <!-- FUENTE PARA TITULO-SPAN -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+DE+SAS:wght@100..400&display=swap" rel="stylesheet">
+    <!-- FUENTE PARA UBICACIÓN-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=BBH+Sans+Bartle&display=swap" rel="stylesheet">
+    <!-- FUENTE PARA BOTON JUGAR -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gravitas+One&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/styles.css" as="style">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <h1 class="titulo">Quiniela <span>Liga MX</span></h1>
+    </header>
+
+    <div class="nav-bg">
+        <nav class="navegacion-principal contenedor">
+            <a href="index.php">Inicio</a>
+            <a href="#">Sobre Mi</a>
+            <a href="participantes.php">Participantes</a>
+            <a href="#">Partidos</a>
+            <a href="#">Contacto</a>
+        </nav>
+    </div>
+    
+    
+<!-- INICIA SLIDER RESPONSIVO -->
+
+<div class="logos" id="logos">
+    <div class="logos-slide" id="slider">
+        <img src="./img/America.png" alt="">
+        <img src="./img/Atlas.png" alt="">
+        <img src="./img/AtleticoSanLuis.png" alt="">
+        <img src="./img/Chivas.png" alt="">
+        <img src="./img/ClubDeportivoToluca.png" alt="">
+        <img src="./img/ClubSantos.png" alt="">
+        <img src="./img/ClubTijuana.png" alt="">
+        <img src="./img/CruzAzul.png" alt="">
+        <img src="./img/FCJuarez.png" alt="">
+        <img src="./img/Leon.png" alt="">
+        <img src="./img/Mazatlan.png" alt="">
+        <img src="./img/Monterrey.png" alt="">
+        <img src="./img/Necaxa.png" alt="">
+        <img src="./img/Pachuca.png" alt="">
+        <img src="./img/Puebla.png" alt="">
+        <img src="./img/Queretaro.png" alt="">
+        <img src="./img/Tigres.png" alt="">
+    </div>
+</div>
+
+
+
+<script src="./js/app.js"></script>
+
+<!-- TERMINA SLIDER RESPONSIVO -->
+
+
+
+</main>
+    
+<footer class="footer">
+    <p>Todos los derechos reservados. HCV</p>
+</footer>
+
+</body>
+</html>
